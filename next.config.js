18393dccbb2802156faaebf91aefcf6b02ee2382
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2022-05-20 17:54:13 +0800

    Sidebar

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'openseauserdata.com',
+      'brand.assets.adidas.com',
+      'media0.gliph.com',
+      'avatars.dicebear.com',
+      'media1.gliph.com',
+      'media2.gliph.com',
+      'media3.gliph.com',
+      'media4.gliph.com',
+    ]
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
+import { MoralisProvider } from 'react-moralis'
+import { AmazonProvider } from '../context/AmazonContext';
+
+const serverUrl = process.env.NEXT_PUBLIC_MORALIS_SERVER;
+const appId = process.env.NEXT_PUBLIC_MORALIS_APP_ID;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl={serverUrl}
+      appId={appId}
+    >
+      <AmazonProvider>
+        <Component {...pageProps} />
+      </AmazonProvider>
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
