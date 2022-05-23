d2d5e2b224be3541ee9659426b3dbae1a7907bf0
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2022-05-24 02:37:26 +0800

    Add header

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import { MoralisProvider } from 'react-moralis'
 import { AmazonProvider } from '../context/AmazonContext';
+import { ModalProvider } from 'react-simple-hook-modal';
 
 const serverUrl = process.env.NEXT_PUBLIC_MORALIS_SERVER;
 const appId = process.env.NEXT_PUBLIC_MORALIS_APP_ID;
@@ -12,7 +13,9 @@ function MyApp({ Component, pageProps }) {
       appId={appId}
     >
       <AmazonProvider>
-        <Component {...pageProps} />
+        <ModalProvider>
+          <Component {...pageProps} />
+        </ModalProvider>
       </AmazonProvider>
     </MoralisProvider>
   )
